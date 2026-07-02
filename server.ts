8a14ac6fe66e2dfb74ced5210a25f558e148637a
User <user@example.com>
2026-07-03 01:12:02 +0545

    Fix: change port to 3055 on firebase-auth-test branch

--- server.ts
+++ server.ts
@@ -78,7 +78,7 @@ const transporter = nodemailer.createTransport({
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3055;
 
   app.use(cors());
   app.use(express.json());
